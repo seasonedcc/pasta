0c61766ef852d33507c7fae0e1be49417e0cbcd6
Diogo Biazus <diogo@biazus.ca>
2022-06-09 13:48:14 -0400

    Use package mappings to install postgres and pgsql-ast-parser from npm

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -12,16 +12,22 @@ await build({
     // see JS docs for overview and more options
     deno: true,
   },
+  mappings: {
+    "https://deno.land/x/postgresjs@v3.2.4/mod.js": {
+      name: "postgres",
+      version: "^3.2.4",
+    },
+    "https://deno.land/x/pgsql_ast_parser@10.2.0/mod.ts": {
+      name: "pgsql-ast-parser",
+      version: "^10.2.0",
+    },
+  },
   package: {
     // package.json properties
     name: "pasta",
     version: Deno.args[0],
     description: "Your package.",
     license: "MIT",
-    "dependencies": {
-      "postgres": "^3.2.4",
-      "pgsql-ast-parser": "^10.3.1",
-    },
   },
 });
 
