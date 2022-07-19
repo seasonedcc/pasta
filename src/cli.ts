f5f137f0080618bb95dc0d4ee1972b1e3063d202
Diogo Biazus <diogo@biazus.ca>
2022-07-19 14:53:40 -0400

    Add a template for the cli so we can start implementing the client generator

---
+++ src/cli.ts
@@ -0,0 +1,9 @@
+import { parse } from "https://deno.land/std@0.148.0/flags/mod.ts";
+import postgres from "https://deno.land/x/postgresjs@v3.2.4/mod.js";
+
+const { connectionUrl } = parse(Deno.args);
+
+console.info(`Connecting to ${connectionUrl} ...`);
+const sql = postgres(connectionUrl);
+
+console.info("Done ✅");
