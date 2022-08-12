d37e20a5599e22425e2b4a6e8a5d5694aa42aa83
Diogo Biazus <diogo@biazus.ca>
2022-08-12 11:45:21 -0400

    Connect using proper connection url from parameters

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,14 @@
 import { parse } from "https://deno.land/std@0.148.0/flags/mod.ts";
 import postgres from "https://deno.land/x/postgresjs@v3.2.4/mod.js";
 
-const { connectionUrl } = parse(Deno.args);
+const { "_": connectionUrl } = parse(Deno.args);
 
-console.info(`Connecting to ${connectionUrl} ...`);
-const sql = postgres(connectionUrl);
+const sql = postgres(`${connectionUrl}`);
 
-const version = await sql`select version()`;
-console.info(`Connected to${version}`);
+const [{ current_database, version }] = await sql
+  `select current_database(), version()`;
 
+console.info(`Connected to ${current_database}@${version}`);
+
+await sql.end();
 console.info("Done ✅");
