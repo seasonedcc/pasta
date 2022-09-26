a99aa9f2ccbe96faaf6933e285bb0f0285bcc6e4
Diogo Biazus <diogo@biazus.ca>
2022-09-26 11:13:43 -0400

    Extract withTestDatabase helper so we can reuse to test schema generator

---
+++ src/__tests__/test-database.ts
@@ -0,0 +1,33 @@
+import postgres from "https://deno.land/x/postgresjs@v3.2.4/mod.js";
+
+function withTestDatabase(
+  testCase: (sql: postgres.Sql<{}>) => Promise<void>,
+): () => Promise<void> {
+  return async () => {
+    const uri = "postgres://localhost/pasta_test";
+    Deno.env.set("DATABASE_URL", uri);
+    const sql = postgres(uri);
+    await sql`DROP TABLE IF EXISTS public."user", public.account, public.user_account CASCADE`;
+    await sql`CREATE TABLE public."user" (
+        id serial PRIMARY KEY,
+        data text NOT NULL,
+        created_at timestamp NOT NULL DEFAULT now(),
+        tags jsonb NOT NULL DEFAULT '[]'
+      )`;
+    await sql`CREATE TABLE public.account (
+      id serial PRIMARY KEY,
+      name text NOT NULL,
+      created_at timestamp NOT NULL DEFAULT now()
+    )`;
+    await sql`CREATE TABLE public.user_account (
+      id serial PRIMARY KEY,
+      user_id integer NOT NULL REFERENCES "user",
+      account_id integer NOT NULL REFERENCES public.account,
+      created_at timestamp NOT NULL DEFAULT now()
+    )`;
+
+    return testCase(sql);
+  };
+}
+
+export { withTestDatabase };
